3f2ca5f79cdd760ca1a55212264d2ceaf29e823a
LucasGonzaloR03 <lucasgonzalorodriguez9@gmail.com>
2026-06-05 20:57:55 -0300

    Ajusta el estilo de Login y App para asegurar un mínimo de altura del 100vh; mejora la estructura del formulario y la gestión de errores.

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -9,7 +9,6 @@ import { UserLoginData } from "../../types/usuario";
 import { loginService } from "../../services/LoginService";
 import { useOnInit } from "../../utils/hooks";
 import { Spinner } from "../../components/Spinner";
-// FIX: claves importadas desde UsuarioService, no duplicadas acá
 import {
   USER_KEY_ID_STORAGE,
   USER_KEY_TIPO_STORAGE,
@@ -18,9 +17,7 @@ import {
   USER_KEY_TOKEN_REFRESH,
 } from "../../services/UsuarioService";
 
-
 const Login = () => {
-
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('')
@@ -33,17 +30,15 @@ const Login = () => {
   const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
 
   const snackbarOpen = !!errorMessage
-
   const navigate = useNavigate()
-
   const handleNavigation = (path: string) => { navigate(path); }
 
   const ingresar = async () => {
     setFromTouched(true);
     if (!userName || !password) {
       setErrorMessage(
-        !userName && !password? 'El nombre de usuario y la contraseña están vacíos': 
-        !userName? 'El nombre de usuario está vacío': 'La contraseña está vacía'
+        !userName && !password ? 'El nombre de usuario y la contraseña están vacíos' :
+        !userName ? 'El nombre de usuario está vacío' : 'La contraseña está vacía'
       );
       return;
     }
@@ -56,115 +51,114 @@ const Login = () => {
       sessionStorage.setItem(USER_KEY_FOTO_STORAGE, usuarioLogueado.fotoPerfil.toString())
       sessionStorage.setItem(USER_KEY_TOKEN_ACCESS, usuarioLogueado.tokenAcceso)
       sessionStorage.setItem(USER_KEY_TOKEN_REFRESH, usuarioLogueado.tokenRefresco)
-      
       switch (usuarioLogueado.tipoUsuario) {
-        case "CHOFER":
-            handleNavigation('/homechofer')
-            break
-        case "PASAJERO":
-            handleNavigation('/homepasajero')
-            break
-        default:
-            handleNavigation('/login')
+        case "CHOFER": handleNavigation('/homechofer'); break
+        case "PASAJERO": handleNavigation('/homepasajero'); break
+        default: handleNavigation('/login')
       }
-
     } catch (error: unknown) {
       mostrarMensajeError(error as ErrorResponse, setErrorMessage)
-    }
-    finally{
+    } finally {
       setIsLoading(false);
     }
   };
 
-  useOnInit(()=>{sessionStorage.clear()})
+  useOnInit(() => { sessionStorage.clear() })
 
   return (
-    <Container className="body">
-      <Box className="login-container">
-        <div className="login-logo-container">
-          <UbertoLogo tamaño="3em" colorLogo="#4e199e"></UbertoLogo>
-        </div>
-        
-        <form >
-        <FormControl className="login-form" sx={{ m: 1, width: '25ch', gap: 1.2, color:'#7B4BDF'}} variant="outlined">
+    // FIX: centrado manejado con sx de MUI, sin depender de clase CSS externa
+    <Container sx={{
+      minHeight: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Box sx={{ mb: 1 }}>
+          <UbertoLogo tamaño="3em" colorLogo="#4e199e" />
+        </Box>
 
-        <TextField 
-          label="Username" 
-          fullWidth  
-          margin="normal" 
-          color="primary" 
-          name="titulo" 
-          required
-          onChange={handleUsername}
-          error={fromTouched && !userName}
-          helperText={
-            fromTouched && !userName ? (
-              <Box display="flex" alignItems="center">
-                <Typography color="red" fontSize={12}>El usuario es obligatorio</Typography>
-              </Box>
-            ) : ""
-          }
-          sx={{
-              '& label.Mui-focused': { color: '#4e199e' }, 
-              '& .MuiOutlinedInput-root': {
-                  '& fieldset': { borderColor: '#4e199e' }, 
-                  '&:hover fieldset': { borderColor: '#7a3eb1' }, 
-                  '&.Mui-focused fieldset': { borderColor: '#4e199e' } 
+        <form>
+          <FormControl sx={{ m: 1, width: '25ch', gap: 1.2, color: '#7B4BDF' }} variant="outlined">
+            <TextField
+              label="Username"
+              fullWidth
+              margin="normal"
+              color="primary"
+              name="username"
+              required
+              onChange={handleUsername}
+              error={fromTouched && !userName}
+              helperText={
+                fromTouched && !userName ? (
+                  <Box display="flex" alignItems="center">
+                    <Typography color="red" fontSize={12}>El usuario es obligatorio</Typography>
+                  </Box>
+                ) : ""
               }
-          }}
-        />
-  
-        <FormControl variant="outlined" fullWidth required margin="normal" error={fromTouched && !password}
-          sx={{
-            '& label.Mui-focused': { color: '#4e199e' }, 
-            '& .MuiOutlinedInput-root': {
-                '& fieldset': { borderColor: '#4e199e' }, 
-                '&:hover fieldset': { borderColor: '#7a3eb1' }, 
-                '&.Mui-focused fieldset': { borderColor: '#4e199e' } 
-            }
-          }}
-        >
-        <InputLabel htmlFor="password-input">Contraseña</InputLabel>
-          <OutlinedInput
-          id="password-input"
-          type={showPassword ? 'text' : 'password'}
-          autoComplete="current-password"
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton
-                aria-label={showPassword ? 'Ocultar contraseña' : 'Mostrar contraseña'}
-                onClick={handleClickShowPassword}
-                edge="end"
-              >
-                {showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
-          }
-          onChange={handlePassword}
-          label="Contraseña" 
-        />
-        
-        {fromTouched && !password && ( <FormHelperText>La contraseña es obligatoria</FormHelperText>)}
-        </FormControl>
-          <Button onClick={ingresar} variant="contained" size="large" sx={{ backgroundColor:"#4e199e",marginTop: 1, fontFamily: ['Roboto', 'sans-serif'], textTransform: 'none' }}>Ingresar</Button>
-          <Button 
-            onClick={() => navigate('/register')} 
-            sx={{ color: "#4e199e", textTransform: 'none', fontSize: '0.8rem' }}
-          >
-            ¿No tienes una cuenta? Registrate aquí
-          </Button>
-        </FormControl>
+              sx={{
+                '& label.Mui-focused': { color: '#4e199e' },
+                '& .MuiOutlinedInput-root': {
+                  '& fieldset': { borderColor: '#4e199e' },
+                  '&:hover fieldset': { borderColor: '#7a3eb1' },
+                  '&.Mui-focused fieldset': { borderColor: '#4e199e' }
+                }
+              }}
+            />
+
+            <FormControl variant="outlined" fullWidth required margin="normal" error={fromTouched && !password}
+              sx={{
+                '& label.Mui-focused': { color: '#4e199e' },
+                '& .MuiOutlinedInput-root': {
+                  '& fieldset': { borderColor: '#4e199e' },
+                  '&:hover fieldset': { borderColor: '#7a3eb1' },
+                  '&.Mui-focused fieldset': { borderColor: '#4e199e' }
+                }
+              }}
+            >
+              <InputLabel htmlFor="password-input">Contraseña</InputLabel>
+              <OutlinedInput
+                id="password-input"
+                type={showPassword ? 'text' : 'password'}
+                autoComplete="current-password"
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label={showPassword ? 'Ocultar contraseña' : 'Mostrar contraseña'}
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                onChange={handlePassword}
+                label="Contraseña"
+              />
+              {fromTouched && !password && (<FormHelperText>La contraseña es obligatoria</FormHelperText>)}
+            </FormControl>
+
+            <Button onClick={ingresar} variant="contained" size="large"
+              sx={{ backgroundColor: "#4e199e", marginTop: 1, fontFamily: ['Roboto', 'sans-serif'], textTransform: 'none' }}
+            >
+              Ingresar
+            </Button>
+            <Button onClick={() => navigate('/register')}
+              sx={{ color: "#4e199e", textTransform: 'none', fontSize: '0.8rem' }}
+            >
+              ¿No tienes una cuenta? Registrate aquí
+            </Button>
+          </FormControl>
         </form>
       </Box>
-      
-      <div className="login-snackbar">
-        <Notificacion
-          open={snackbarOpen}
-          mensaje={errorMessage}
-          severidad="error"
-          onClose={() => setErrorMessage('')}
-        />
-      </div>
+
+      <Notificacion
+        open={snackbarOpen}
+        mensaje={errorMessage}
+        severidad="error"
+        onClose={() => setErrorMessage('')}
+      />
       <Spinner color="#4e199e" size={50} isLoading={isLoading} />
     </Container>
   );
